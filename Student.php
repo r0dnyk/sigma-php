c1167ee47dd588682a4e335c54a29600a45ab62d
valnative <valerii.yevdochenko@gmail.com>
2022-04-28 11:40:52 +0300

    Done

---
+++ Student.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace University;
+
+class Student
+{
+    protected string $firstName;
+    protected string $lastName;
+    protected int $group;
+    protected int $mark;
+    protected bool $isScientificWork = false;
+
+
+    public function __construct(string $firstName, string $lastName, int $group, int $mark)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->group = $group;
+        $this->mark = $mark;
+    }
+
+    public function getScholarship(): int
+    {
+        return $this->mark === 5 ? 100 : 80;
+    }
+
+}
+
+class Aspirant extends Student
+{
+    protected bool $isScientificWork = true;
+
+    public function getScholarship(): int
+    {
+        return $this->mark === 5 ? 200 : 180;
+    }
+}
+
+$student = [
+    new Student('Jack', 'London', 22, 5),
+    new Aspirant('Alan', 'Parker', 14, 5)
+];
+
+
+foreach ($student as $value) {
+    echo $value->getScholarship() . PHP_EOL;
+}
